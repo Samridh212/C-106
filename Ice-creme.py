48e5e1b5c61f7cdb676c11c242504a67ba77cd54
Samridh212 <70936587+Samridh212@users.noreply.github.com>
2021-09-30 15:54:54 +0530

    Add files via upload

---
+++ Ice-creme.py
@@ -0,0 +1,8 @@
+import csv
+import pandas as pd
+import plotly.express as px
+
+with open("./data/Ice-Cream vs Cold-Drink vs Temperature - Ice Cream Sale vs Temperature data.csv") as csv_file:
+    df=csv.DictReader(csv_file)
+    fig=px.scatter(df,x="Temperature",y="Ice-cream Sales")
+    fig.show()
